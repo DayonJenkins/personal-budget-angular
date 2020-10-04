3f7321f10e7806ad3b9fefaed3bdc9105f4bb7ab
DayonJ <djenki44@uncc.edu>
2020-10-04 12:11:50 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const budget = require('./budget.json');
 const app = express();
 const port = 3000;
 
+
+
 app.use('/', express.static('public'));
 
-app.get('/index', (req, res) =>{
+
+app.get('/hello', (req, res) =>{
     res.send('Hello World!');
 });
+
+app.get('/budget', (req, res) =>{
+    res.json(budget);
+});
+
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
